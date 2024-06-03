b78729f547852a6e3ad1981a30931ba5e76f0e80
Jose SR <josesanchez09190@gmail.com>
2024-06-03 02:14:41 -0600

    upgrade search by title of book

--- books/src/main/java/com/jsr_books/books/main/MenuMain.java
+++ books/src/main/java/com/jsr_books/books/main/MenuMain.java
@@ -5,12 +5,15 @@ import com.jsr_books.books.model.SearchResult;
 import com.jsr_books.books.service.ApiService;
 import com.jsr_books.books.service.ConvertData;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class MenuMain {
 
+    private static final String URL_BASE = "https://gutendex.com/";
     private final ApiService apiService = new ApiService();
     private final ConvertData convertData = new ConvertData();
 
@@ -36,7 +39,7 @@ public class MenuMain {
 
                     // search book by title
                     String bookName = getUserInput(String.class, "Ingresa el nombre del libro que deseas ver: ");
-                    getBookByTitle(searchResultData, bookName);
+                    getBookByTitle(bookName);
 
                     // get statistics
                     getStatistics(searchResultData);
@@ -65,10 +68,18 @@ public class MenuMain {
         System.out.println("Libro con mayor descarga: " + est.getMax());
         System.out.println("Libro con menor descarga: " + est.getMin());
         System.out.println("Media de libros descargadas: " + est.getAverage());
+        System.out.println("Registros evaluados para las estadisticas: " + est.getCount());
     }
 
-    private void getBookByTitle(SearchResult resultData, String bookName) {
-        Optional<Book> searchBookByTitle = resultData.results().stream()
+    private void getBookByTitle(String bookName) {
+
+        String bookNameResult = encodeAndFormatSeriesName(bookName);
+        String url = URL_BASE + "books/?search=" + bookNameResult;
+
+        String json = apiService.getData(url);
+        var searchData = convertData.getData(json, SearchResult.class);
+
+        Optional<Book> searchBookByTitle = searchData.results().stream()
                 .filter(b -> b.title().toUpperCase().contains(bookName.toUpperCase()))
                 .findFirst();
 
@@ -89,7 +100,7 @@ public class MenuMain {
                 .sorted(Comparator.comparingInt(Book::downloadCount).reversed())
                 .limit(topBooks)
                 .forEach(book -> {
-                    System.out.println(ind.getAndIncrement() + "-> Title: " + book.title());
+                    System.out.println(ind.getAndIncrement() + "-> Title: " + book.title().toUpperCase());
                     System.out.println("--- Total descargas: " + book.downloadCount());
                 });
     }
@@ -103,8 +114,7 @@ public class MenuMain {
     }
 
     private SearchResult fetchResultData() {
-        String BASE_URL = "https://gutendex.com/";
-        String url = BASE_URL + "books/";
+        String url = URL_BASE + "books/";
         String json = apiService.getData(url);
         return convertData.getData(json, SearchResult.class);
     }
@@ -123,5 +133,8 @@ public class MenuMain {
         throw new IllegalArgumentException("Tipo de datos no soportado: " + dataType.getSimpleName());
     }
 
-
+    private String encodeAndFormatSeriesName(String seriesName) {
+        String encodedSeriesName = URLEncoder.encode(seriesName, StandardCharsets.UTF_8);
+        return encodedSeriesName.replace("+", "%20");
+    }
 }
--- books/src/main/java/com/jsr_books/books/model/SearchResult.java
+++ books/src/main/java/com/jsr_books/books/model/SearchResult.java
@@ -1,9 +1,11 @@
 package com.jsr_books.books.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record SearchResult(
         @JsonAlias("count") Integer count,
         @JsonAlias("next") String next,
